35cb7f2911fa5acfc134b836402368fde52a718a
Alon Albalak <alonalbalak@gmail.com>
2021-06-05 12:59:52 -0700

    fix typo

--- get_prepare_CORD.sh
+++ get_prepare_CORD.sh
@@ -10,8 +10,10 @@ tar -xf document_parses.tar.gz
 rm changelog cord_19_embeddings.tar.gz document_parses.tar.gz
 
 popd
+python3 -m nltk.downloader punkt
 python3 splitCORD.py \
-    --metadata-file=$CORD_VERSION/metadata.CORD_VERSION
-    --pmc-path=$CORD_VERSION/document_parses/pmc_json
+    --metadata-file=$CORD_VERSION/metadata.csv \
+    --pmc-path=$CORD_VERSION/document_parses/pmc_json \
     --out-path=$CORD_VERSION/CORD19_corpus.jsonl
 
+rm $CORD_VERSION/document_parses $CORD_VERSION/metadata.csv
