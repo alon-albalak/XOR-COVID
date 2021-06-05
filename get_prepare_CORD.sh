440febe816da8b811cc00d733927e0ae5f131d37
Alon Albalak <alonalbalak@gmail.com>
2021-06-04 22:48:09 -0700

    add CORD19 prep script

---
+++ get_prepare_CORD.sh
@@ -0,0 +1,17 @@
+CORD_VERSION=2021-05-31
+
+mkdir CORD19
+wget https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/historical_releases/cord-19_$CORD_VERSION.tar.gz
+tar -xf cord-19_$CORD_VERSION.tar.gz
+rm cord-19_$CORD_VERSION.tar.gz
+pushd $CORD_VERSION
+tar -xf document_parses.tar.gz
+
+rm changelog cord_19_embeddings.tar.gz document_parses.tar.gz
+
+popd
+python3 splitCORD.py \
+    --metadata-file=$CORD_VERSION/metadata.CORD_VERSION
+    --pmc-path=$CORD_VERSION/document_parses/pmc_json
+    --out-path=$CORD_VERSION/CORD19_corpus.jsonl
+
