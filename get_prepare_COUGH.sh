f891d49f452562a1c3943257c1c0d578063fe116
Alon Albalak <alonalbalak@gmail.com>
2021-06-18 16:19:41 -0700

    fix typo

--- get_prepare_COUGH.sh
+++ get_prepare_COUGH.sh
@@ -4,5 +4,5 @@ wget https://github.com/sunlab-osu/covid-faq/archive/refs/heads/main.tar.gz
 mkdir COUGH
 tar -xzf main.tar.gz -C COUGH
 
-python3 splitCOUGH --save_path=COUGH --data_path=COUGH/covid-faq-main/data/FAQ_Bank_eval.csv
+python3 splitCOUGH.py --save_path=COUGH --data_path=COUGH/covid-faq-main/data/FAQ_Bank_eval.csv
 rm -r COUGH/covid-faq-main
