15434cac1f5548fa37ac73010b96bac1f5fc61cc
Alon Albalak <alonalbalak@gmail.com>
2021-06-19 12:02:54 -0700

    remove zipped directory after unzipping

--- get_prepare_COUGH.sh
+++ get_prepare_COUGH.sh
@@ -3,6 +3,7 @@ wget https://github.com/sunlab-osu/covid-faq/archive/refs/heads/main.tar.gz
 
 mkdir COUGH
 tar -xzf main.tar.gz -C COUGH
+rm main.tar.gz
 
 python3 splitCOUGH.py --save_path=COUGH --data_path=COUGH/covid-faq-main/data/FAQ_Bank_eval.csv
 rm -r COUGH/covid-faq-main
