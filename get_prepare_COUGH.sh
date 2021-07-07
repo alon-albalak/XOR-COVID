369e929b758467d460f09221fc8c60b48e19a6c6
Alon Albalak <alonalbalak@gmail.com>
2021-07-07 11:15:00 -0700

    use multilingual COUGH data

--- get_prepare_COUGH.sh
+++ get_prepare_COUGH.sh
@@ -5,5 +5,5 @@ mkdir COUGH
 tar -xzf main.tar.gz -C COUGH
 rm main.tar.gz
 
-python3 splitCOUGH.py --save_path=COUGH --data_path=COUGH/covid-faq-main/data/FAQ_Bank_eval.csv
+python3 splitCOUGH.py --save_path=COUGH --data_path=COUGH/covid-faq-main/data/FAQ_Bank.csv
 rm -r COUGH/covid-faq-main
