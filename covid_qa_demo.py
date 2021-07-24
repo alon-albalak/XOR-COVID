110eb86e9151aa183bf287483fec65e912c94527
Alon Albalak <alonalbalak@gmail.com>
2021-07-24 14:08:41 -0700

    decode translated tokens

--- covid_qa_demo.py
+++ covid_qa_demo.py
@@ -390,8 +390,10 @@ if __name__ =='__main__':
             translation = ""
             if doc["language"] == "spa":
                 translation = mt_model_es.generate(**mt_tokenizer_es(answers[count], return_tensors="pt"))
+                translation = mt_tokenizer_es.decode(translation[0], skip_special_tokens=True)
             if doc["language"] == "chi":
                 translation = mt_model_zh.generate(**mt_tokenizer_zh(answers[count], return_tensors="pt"))
+                translation = mt_tokenizer_zh.decode(translation[0], skip_special_tokens=True)
             with st.beta_expander("{}, {}".format(doc['journal'], doc['date'])):
                 st.markdown('**Title:** {}'.format(doc['title']))
                 st.markdown('**Language:** {}'.format(doc['language']))
